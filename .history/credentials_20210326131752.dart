4aa24aeac3129f4e23315281f556fb845d005bd0
Tristan Sutton <tristan.sutton@gmail.com>
2021-04-29 17:16:29 +1000

    Delete .history directory

--- .history/credentials_20210326131752.dart
+++
@@ -1,3 +0,0 @@
-class Credentials{
-  static 
-}
--- .history/lib/credentials_20210331124302.dart
+++
@@ -1,6 +0,0 @@
-class Credentials {
-  static String clientID =
-      '810856152432-pfbs6p2796mdmdonsr9g1u33im89jv9m.apps.googleusercontent.com';
-  static String clientSecret = 
-    ''
-}
