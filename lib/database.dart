f1d35551d9ed8c5fbc595ef48654bc358b792f5d
Tristan Sutton <tristan.sutton@gmail.com>
2021-04-30 15:22:51 +1000

    create methods for tbl_user and tbl_booking

--- lib/database.dart
+++ lib/database.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'package:meta/meta.dart';
 
 import 'package:mysql1/mysql1.dart';
+import 'package:smart_parking_solutions_common/smart_parking_solutions_common.dart';
 import 'package:smart_parking_solutions_rest/credentials.dart';
 
 class DataBase {
@@ -17,11 +19,38 @@ class DataBase {
     return conn;
   }
 
-  static initDB() async {
+  static void initDB() async {
     final conn = await getConnection();
     await conn.query(
-        'CREATE TABLE IF NOT EXISTS tbl_user userID int NOT NULL AUTO_INCREMENT PRIMARY KEY, givenName varchar(255), familyName varchar(255), email varchar(255) disabled boolean');
+        'CREATE TABLE IF NOT EXISTS tbl_user (userID int NOT NULL AUTO_INCREMENT PRIMARY KEY, givenName varchar(255), familyName varchar(255), email varchar(255), handicapped boolean)');
     await conn.query(
-        'CREATE TABLE IF NOT EXISTS tbl_booking id int NOT NULL AUTO_INCREMENT PRIMARY KEY');
+        'CREATE TABLE IF NOT EXISTS tbl_booking (bookingID int NOT NULL AUTO_INCREMENT PRIMARY KEY, bayID varchar(255), createdDate DATETIME, startDate DATETIME, endDate DATETIME, ownerFK int, FOREIGN KEY (ownerFK) REFERENCES tbl_user(userID))');
+    await conn.close();
+  }
+
+  static Future<Results> createUser(
+      {@required String givenName,
+      @required String familyName,
+      @required String email,
+      @required bool handicapped}) async {
+    final conn = await getConnection();
+    final result = conn.query(
+        'insert into tbl_user (givenName, familyName, email, handicapped) values(?, ?, ?, ?)',
+        [givenName, familyName, email, handicapped]);
+    await conn.close();
+    return result;
+  }
+
+  static Future<Results> createBooking(
+      {@required String bayID,
+      @required DateTime startDate,
+      @required DateTime endDate,
+      @required User owner}) async {
+    final conn = await getConnection();
+    final result = conn.query(
+        'insert into tbl_booking (bayID, createdDate, startDate, endDate, ownerFK) values(?, ?, ?, ? ,?)',
+        [bayID, DateTime.now(), startDate, endDate, owner.userID]);
+    await conn.close();
+    return result;
   }
 }
