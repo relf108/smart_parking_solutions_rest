2035e4b4b75a64d0730fa00c3732fe320bc36a3d
Tristan Sutton <tristan.sutton@gmail.com>
2021-04-30 13:59:48 +1000

    comments

--- lib/channel.dart
+++ lib/channel.dart
@@ -4,32 +4,20 @@ import 'package:smart_parking_solutions_rest/credentials.dart';
 
 import 'smart_parking_solutions_rest.dart';
 
-/// This type initializes an application.
-///
-/// Override methods in this class to set up routes and initialize services like
-/// database connections. See http://aqueduct.io/docs/http/channel/.
+
 class SmartParkingSolutionsRestChannel extends ApplicationChannel {
-  /// Initialize services in this method.
-  ///
-  /// Implement this method to initialize services, read values from [options]
-  /// and any other initialization required before constructing [entryPoint].
-  ///
-  /// This method is invoked prior to [entryPoint] being accessed.
+
   @override
   Future prepare() async {
     logger.onRecord.listen(
         (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
   }
 
-  /// Construct the request channel.
-  ///
-  /// Return an instance of some [Controller] that will be the initial receiver
-  /// of all [Request]s.
-  ///
-  /// This method is invoked after [prepare].
   @override
   Controller get entryPoint {
     final router = Router();
+
+    ///This channel is used when performing Google OAuth registration.
     router.route("/authUser").linkFunction((request) async {
       var responseCode = [];
       var accessToken = '';
@@ -51,6 +39,8 @@ class SmartParkingSolutionsRestChannel extends ApplicationChannel {
       }
       accessToken =
           accessToken.split('access_token')[1].split(':')[1].split('\"')[1];
+
+      ///push to db along with user
       print(accessToken);
       return Response.ok({"key": "value"});
     });
