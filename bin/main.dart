2f539c5ff9d65d83718b20c0461ea1072695c365
Tristan Sutton <tristan.sutton@gmail.com>
2021-08-30 17:29:55 +1000

    reverted number of initial server threads to 2 so that other isolates can run on their own core

--- bin/main.dart
+++ bin/main.dart
@@ -5,7 +5,7 @@ Future main() async {
     ..options.configurationFilePath = "config.yaml"
     ..options.port = 8888;
 
-  final count = Platform.numberOfProcessors;
+  final count = Platform.numberOfProcessors ~/ 2;
   await app.start(numberOfInstances: count > 0 ? count : 1);
 
   try {
